ff648fce1dadd3b8706808dd0d129ef1b2aeea00
Abhijeet Tiwari <abhijeett.tagline@gmail.com>
2023-08-02 16:38:26 +0530

    fifth commit

---
+++ question5.py
@@ -0,0 +1,11 @@
+numbers = [9,4,8,10,2,4,8,3,14,4,8]
+
+num = 12
+
+pairs = []
+
+for i in range(len(numbers)):
+    for j in range(len(numbers)):
+        if numbers[i]+numbers[j]== num and sorted([numbers[i],numbers[j]]) not in pairs:
+            pairs.append(sorted([numbers[i],numbers[j]]))
+print((pairs))
