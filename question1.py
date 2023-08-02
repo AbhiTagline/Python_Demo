94ea9d86ac693d1fb6b63d1db69f0ad801813c24
Abhijeet Tiwari <abhijeett.tagline@gmail.com>
2023-08-02 15:43:06 +0530

    first commit

--- question1.py
+++ question1.py
@@ -1,22 +1,15 @@
 # Question1 
 
 choice = input("A. Length of list\nB. Display first 3 numbers\nC. Display sum of odd numbers\nD. Number of duplicate numbers\nE. Display list without duplicate numbers\nEnter Your choice: ")
-lst = [2,4,5,2,12,44,5,1,2,3]
+numbers  = [2,4,5,2,12,44,5,1,2,3]
 if choice.lower()=="a":
-    print("Length of list: ",len(lst))
+    print("Length of list: ",len(numbers))
 elif choice.lower()=="b":
-    print("First 3 numbers: ",lst[:3])
+    print("First 3 numbers: ",numbers[:3])
 elif choice.lower()=="c":
-    print("Sum of odd numbers",sum([x for x in lst if x%2!=0]))
+    print("Sum of odd numbers",sum([x for x in numbers if x%2!=0]))
 elif choice.lower()=="d":
-    nlst = []
-    result = []
-    for i in lst:
-        if i not in nlst:
-            nlst.append(i)
-        else:
-            result.append(i) 
-            unique_elements = set(result[::])
-    print("Number of duplicate numbers: ",len(unique_elements),tuple(unique_elements),"repeats")
+    ducplicates_numbers = {x :numbers.count(x) for x in numbers if numbers.count(x) >=2 }
+    print("Number of duplicate numbers: ",len(ducplicates_numbers),tuple(ducplicates_numbers.keys()),"repeats")
 elif choice.lower()=="e":
-    print("List without duplicate numbers: ",list(set(lst)))
+    print("List without duplicate numbers: ",list(set(numbers)))
