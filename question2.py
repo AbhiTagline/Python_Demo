0c888862b18c5235668af4cceabef9176ddaf0ee
Abhijeet Tiwari <abhijeett.tagline@gmail.com>
2023-08-02 14:06:23 +0530

    sccond commit

---
+++ question2.py
@@ -0,0 +1,22 @@
+names = ['Smith', 'Johnson', 'Williams', 'Jones', 'Brown', 'Davis', 'Miller', 'Wilson', 'Moore', 'Taylor', 'Anderson', 'Thomas', 'Jackson', 'White']
+print("Names :", names)
+
+
+length = [len(x) for x in names]
+print("Name lengthd:",length)
+
+
+nlst = list(set(length))
+nlst.sort()
+count = 0
+# for i in nlst[:3]:
+#     print(length.count(i),"names of length",i,[x for x in names if len(x) == nlst[count]])
+#     count += 1
+
+
+
+print("The three least frequent name lengths are")
+count = 0
+for i in nlst[-3:]:
+    print(length.count(i),"names of length",i,[x for x in names if len(x) == nlst[count]])
+    count += 1
