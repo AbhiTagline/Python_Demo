1be6d857d7edab68dc4f8cf40fd00296a093a1c3
Abhijeet Tiwari <abhijeett.tagline@gmail.com>
2023-08-02 16:13:56 +0530

    fourth commit

---
+++ question4.py
@@ -0,0 +1,23 @@
+# Question 4
+
+
+def fibo(n):
+    if n<=1:
+        return 1
+    else:
+        return fibo(n-1) + fibo(n-2)
+
+terms = input("Enter the nth term: ")
+for i in range(int(terms)):
+    print(fibo(i))
+
+
+
+
+def sum_of_numbers(lst):
+    if len(lst) == 0:
+        return 0
+    else:
+        return lst[0] + sum_of_numbers(lst[1:])
+
+print(sum_of_numbers([23, 44, 5, 67, 1, 1, 2, 4, 5]))
