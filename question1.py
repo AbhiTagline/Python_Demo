d1592bff9f89d70798119a3f08c5f1310042de29
Abhijeet Tiwari <abhijeett.tagline@gmail.com>
2023-08-03 10:49:16 +0530

    first commit

--- question1.py
+++ question1.py
@@ -1,5 +1,7 @@
 # Question1 
 
+
+from collections import Counter
 choice = input("A. Length of list\nB. Display first 3 numbers\nC. Display sum of odd numbers\nD. Number of duplicate numbers\nE. Display list without duplicate numbers\nEnter Your choice: ")
 numbers  = [2,4,5,2,12,44,5,1,2,3]
 if choice.lower()=="a":
@@ -9,7 +11,13 @@ elif choice.lower()=="b":
 elif choice.lower()=="c":
     print("Sum of odd numbers",sum([x for x in numbers if x%2!=0]))
 elif choice.lower()=="d":
-    ducplicates_numbers = {x :numbers.count(x) for x in numbers if numbers.count(x) >=2 }
-    print("Number of duplicate numbers: ",len(ducplicates_numbers),tuple(ducplicates_numbers.keys()),"repeats")
+    duplicate_numbers = Counter(numbers)
+    duplicate_numbers_lists = [x for x,y in duplicate_numbers.items() if y>=2]
+    print(duplicate_numbers_lists)
+    print("Number of duplicate numbers: ",len(duplicate_numbers_lists),duplicate_numbers_lists,"repeats")
 elif choice.lower()=="e":
     print("List without duplicate numbers: ",list(set(numbers)))
+
+
+
+
