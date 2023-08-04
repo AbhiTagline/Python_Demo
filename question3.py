14004d0dbca643ec5a1e62d83b565284b839f159
Abhijeet Tiwari <abhijeett.tagline@gmail.com>
2023-08-04 10:21:47 +0530

    third commit

--- question3.py
+++ question3.py
@@ -1,7 +1,8 @@
+from collections import Counter
+
 sentences = ["My name is Ram", "He is a good person", "You should be careful while coding", "He can do better", "The person is mysterious", "Jay Shree Ram", "It is my pen"]
 
 word_tree = [z for x in [y.split(" ") for y in sentences] for z in x]
-print(word_tree)
 
-dict = {x:word_tree.count(x) for x in word_tree }
-print(dict)
+word_count = Counter(word_tree)
+print(word_count)
