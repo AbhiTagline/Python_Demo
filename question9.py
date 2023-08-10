6a78da897613e7a11caee454d81ea1329e11dbe3
Abhijeet Tiwari <abhijeett.tagline@gmail.com>
2023-08-10 10:55:47 +0530

    cahnged whole logic

--- question9.py
+++ question9.py
@@ -1,48 +1,24 @@
-# from datetime import datetime, timedelta
-# start_time_str = input("Enter the start time (YYYY-MM-DD HH:mm): ")
-# end_time_str = input("Enter the end time (YYYY-MM-DD HH:mm): ")
-# # according to the requirement there was a interval that should not be added 
-# # input form the user to not include the specific timing
-# start_str_time = input("Enter the start time (HH:MM): ")
-# end_str_time = input("Enter the end time (HH:MM): ")
-# # string time to match the date and time format
-# start_time = datetime.strptime(start_time_str, "%Y-%m-%d %H:%M")
-# end_time = datetime.strptime(end_time_str, "%Y-%m-%d %H:%M")
-# # string time to match the date and time format
-# start_time_obj = datetime.strptime(start_str_time, "%H:%M")
-# end_time_obj = datetime.strptime(end_str_time, "%H:%M")
-# # first finding the diff and then substracting the diff of the intervals which are not gonna be added
-# c = (end_time - start_time) - (end_time_obj - start_time_obj)
-# print('Difference: ', c)
+import datetime
+import portion as P
 
-
-
-# import portion as P
-# from datetime import datetime, timedelta
-
-# # Get user input for start_time and end_time
-# start_time_str = input("Enter the start time (YYYY-MM-DD HH:MM:SS): ")
-# end_time_str = input("Enter the end time (YYYY-MM-DD HH:MM:SS): ")
-
-# # Convert input strings to datetime objects
-# start_time = datetime.strptime(start_time_str, "%Y-%m-%d %H:%M:%S")
-# end_time = datetime.strptime(end_time_str, "%Y-%m-%d %H:%M:%S")
-
-# # Calculate the time difference after excluding night intervals
-# time_difference = timedelta()
-
-# current_time = start_time
-# while current_time < end_time:
-#     night_interval_start = current_time.replace(hour=0, minute=0, second=0)
-#     night_interval_end = current_time.replace(hour=6, minute=0, second=0)
-#     night_interval = P.closed(night_interval_start, night_interval_end)
-
-#     intersection = night_interval & P.closed(current_time, min(end_time, night_interval_end))
-
-#     for interval in intersection:
-#         time_difference += interval.upper - interval.lower
-
-#     current_time = night_interval_end + timedelta(seconds=1)
-
-# print(f"Time difference (excluding night intervals): {time_difference}")
-
+start_date = datetime.datetime(2023, 1, 1, 10, 0, 0)
+end_date = datetime.datetime(2023, 1, 5, 10, 0, 0)
+main_interval = P.closed(start_date, end_date)
+night_interval_start = datetime.time(0, 0, 0)
+night_interval_end = datetime.time(6, 0, 0)
+night_interval = P.closed(night_interval_start, night_interval_end)
+night_intervals_list = []
+current_date = start_date.date() + datetime.timedelta(days=1)  
+while current_date <= end_date.date():
+    interval_start = datetime.datetime.combine(current_date, night_interval_start)
+    interval_end = datetime.datetime.combine(current_date, night_interval_end)
+    current_night_interval = P.closed(interval_start, interval_end)
+    night_intervals_list.append(current_night_interval)
+    current_date += datetime.timedelta(days=1)
+for interval in night_intervals_list:
+    main_interval -= interval
+total_hours = datetime.timedelta(seconds=0)
+for interval in main_interval:
+    total_hours += interval.upper - interval.lower
+total_hours = total_hours.total_seconds() / 3600
+print(f"Total hours: {total_hours} hours")
